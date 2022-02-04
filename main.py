d993d11d2c2583cec7247b566cf692d54f239d61
Monomin-00000000 <74559842+Monomin-00000000@users.noreply.github.com>
2022-02-04 17:04:20 +0000

    added some directory stuff, now have server specific folders auto created using the server ID for the image roulette pool. Planning to add a way for users to add their own images

--- main.py
+++ main.py
@@ -6,24 +6,53 @@ import random
 import chess
 import pygame
 from chessGame import chessGame
+
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
 chessChannels = []
 
+
 class Media:
     def __init__(self):
-        self.rouletteImages = []
-        imagedir = "ImageRoulette"
-        for file in os.listdir(imagedir):
-            location = os.path.join(imagedir, file)
-            self.rouletteImages.append(location)
+        self.path = "rouletteImages/"
+        self.safePath = "rouletteImages"
+
+    def createDir(self, id):
+        imageDir = self.path + str(id) + "-rouletteImages"
+        os.makedirs(imageDir)
+        print("Dir made")
+
+    def checkDir(self, id):
+        fileIs = False
+        for file in os.listdir(self.safePath):
+            file = file.split("-")
+            file = file[0]
+            file = file.split("/")
+            file = int(file[0])
+            if file == id:
+                fileIs = True
+
+        if not fileIs:
+            self.createDir(id)
+            print(f"Directory created for guild id {id}")
+
+    def fetchImage(self, id):
+        images = []
+        path = self.path + str(id) + "-rouletteImages"
+        for file in os.listdir(path):
+            imagePath = os.path.join(path, file)
+            images.append(imagePath)
+
+        return images
 
 
 media = Media()
 client = commands.Bot(command_prefix="!")
 
-chessOn = False
+rouletteFreeSend = {}
+
+
 @client.event
 async def on_ready():
     print(f" {client.user} has successfully connected to discord")
@@ -32,9 +61,71 @@ async def on_ready():
 
 
 @client.command(pass_context=True)
-async def roulette(msg):
-    await msg.channel.send(file=discord.File(random.choice(media.rouletteImages)))
-    print(f"Roulette image sent to channel {msg.channel.name}")
+async def roulette_free(cxt):
+    try:
+        rouletteFreeSend[cxt.guild.id]
+
+    except KeyError:
+        rouletteFreeSend[cxt.guild.id] = False
+
+    change = False
+    while not change:
+        if not rouletteFreeSend[cxt.guild.id]:
+            rouletteFreeSend[cxt.guild.id] = True
+            await cxt.channel.send("!roulette can now be used anywhere")
+            break
+
+        elif rouletteFreeSend[cxt.guild.id]:
+            rouletteFreeSend[cxt.guild.id] = False
+            await cxt.channel.send("!roulette can now only be used in the roulette channel")
+            break
+
+
+@client.command(pass_context=True)
+async def roulette(cxt):
+    try:
+        rouletteFreeSend[cxt.guild.id]
+
+    except KeyError:
+        rouletteFreeSend[cxt.guild.id] = False
+
+    # above makes sure the server that sent the message is in the server dict
+
+    media.checkDir(cxt.guild.id)  # makes sure there is a dir for each specific server
+
+    if rouletteFreeSend[cxt.guild.id]:  # if you can use anywhere
+        images = media.fetchImage(cxt.guild.id)
+        if len(images) != 0:
+            await cxt.channel.send(file=discord.File(random.choice(images)))  # sends a random image
+            print(f"Roulette image sent to channel {cxt.channel.name}")
+
+        else:
+            await cxt.channel.send("This server has no roulette images, please use !roulette_image and an image"
+                                   " within the same message to upload some!")
+
+    if not rouletteFreeSend[cxt.guild.id]:  # if cannot send anywhere
+        if cxt.channel.name == "roulette":
+            images = media.fetchImage(cxt.guild.id)
+            if len(images) != 0:
+                await cxt.channel.send(file=discord.File(random.choice(images)))  # sends a random image
+                print(f"Roulette image sent to channel {cxt.channel.name}")
+
+            else:
+                await cxt.channel.send("This server has no roulette images, please use !roulette_image and an image"
+                                       " within the same message to upload some!")
+        else:
+            channelExist = False
+            for chn in cxt.guild.text_channels:
+                if chn == "roulette":
+                    channelExist = True
+
+            if channelExist:
+                await cxt.channel.send("Please use this command in the roulette channel! Toggle the need for"
+                                       " a specific channel with !roulette_free")
+
+            else:
+                await cxt.channel.send("Please create a channel called 'roulette' or use the command !roulette_free"
+                                       " to use this command anywhere")
 
 
 @client.command(pass_context=True)
@@ -73,10 +164,6 @@ async def chess_start(cxt):
                 gameEngine.makeMove(playerMove)
                 gameEngine.createImage()
                 await cxt.channel.send(file=discord.File("chessImages/board.png"))
-
-
-
-
 
 
 client.run(TOKEN)
